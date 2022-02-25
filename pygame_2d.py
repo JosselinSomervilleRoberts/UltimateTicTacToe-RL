73978c013bf5474f7d75ff397805f7346b64bb1d
JosselinSomervilleRoberts <josselin.somerville@gmail.com>
2022-02-25 14:09:21 +0100

    Documentation and cleaning up

--- pygame_2d.py
+++ pygame_2d.py
@@ -1,247 +1,29 @@
 import pygame
 import time
-import numpy as np
+from board import Board
 
-def current_milli_time():
-    return round(time.time() * 1000)
 
-start_time = time.time()
+# Initialize game environment
 pygame.init()
-
-
-
-class Board:
-    SIZE = 910
-    BOTTOM_SIZE = 80
-
-    COLOR_BACKGROUND = (255,255,255)
-    COLOR_LARGE_GRID = (50, 50, 50 )
-    COLOR_SMALL_GRID = (120,120,120)
-    WIDTH_LARGE_GRID = 10
-    WIDTH_SMALL_GRID = 5
-
-    COLOR_PLAYER_1 = (255, 0, 0)
-    COLOR_PLAYER_2 = (0, 0, 255)
-    WIDTH_PLAYER_1 = 5
-    WIDTH_PLAYER_2 = 5
-    COLOR_BACKGROUND_PLAYER_1 = (255, 150, 150)
-    COLOR_BACKGROUND_PLAYER_2 = (150, 150, 255)
-    
-    COLOR_BACKGROUND_AVAILABLE = (210,210,210)
-    TIME_BLINK_AVAILABLE = 500 # ms
-    FONT = pygame.font.Font(None, 50)
-
-
-    def checkWinBoard(grid):
-        for i in range(3):
-            if grid[i][0] == grid[i][1] == grid[i][2] != 0:
-                return grid[i][0]
-            if grid[0][i] == grid[1][i] == grid[2][i] != 0:
-                return grid[0][i]
-
-        # Diagonals
-        if grid[0][0] == grid[1][1] == grid[2][2] != 0:
-            return grid[0][0]
-        if grid[0][2] == grid[1][1] == grid[2][0] != 0:
-            return grid[0][2]
-
-        return 0
-
-    def gridIsFull(grid):
-        for ix in range(3):
-            for iy in range(3):
-                if grid[ix][iy] == 0:
-                    return False
-        return True
-
-    def __init__(self):
-        self.reset()
-
-    def reset(self):
-        self.resetGrid()
-        self.currentPlayer = 1
-        self.text = "Player 1 plays"
-        self.textColor = Board.COLOR_PLAYER_1
-        self.state = 0
-        self.possible = [[True for _ in range(3)] for _ in range(3)]
-
-    def resetGrid(self):
-        self.grid = np.zeros((3,3,3,3), dtype=int).tolist()
-        self.largeGrid = np.zeros((3,3), dtype=int).tolist()
-
-    def play(self, ixLarge, iyLarge, ixSmall, iySmall):
-        if self.state != 0:
-            return 1 # Move not aload since the game is already finished
-        if not(self.possible[ixLarge][iyLarge]):
-            return 2 # Move not aload since not in the correct large cell
-        elif self.grid[ixLarge][iyLarge][ixSmall][iySmall] != 0:
-            return 3 # Move not aload since the small cell is already occupied
-
-        self.grid[ixLarge][iyLarge][ixSmall][iySmall] = self.currentPlayer # Play
-        self.currentPlayer = 3 - self.currentPlayer # Swap player
-        self.checkWinLargeCell(ixLarge, iyLarge)
-        self.updatePossible(ixSmall, iySmall)
-
-        # Text
-        if self.state == 0:
-            self.text = "Player " + str(self.currentPlayer) + " plays"
-            self.textColor = Board.COLOR_PLAYER_2
-            if self.currentPlayer == 1:
-                self.textColor = Board.COLOR_PLAYER_1
-
-        return 0
-
-    def click(self, px, py):
-        (ixLarge, iyLarge, ixSmall, iySmall) = Board.getCellFromPx(px, py)
-        self.play(ixLarge, iyLarge, ixSmall, iySmall)
-
-    def checkWinLargeCell(self, ix, iy):
-        g = self.grid[ix][iy]
-        res = Board.checkWinBoard(g)
-        if res != 0:
-            self.largeGrid[ix][iy] = res
-            resWin = Board.checkWinBoard(self.largeGrid)
-            if resWin != 0: # A player won
-                self.state = resWin
-
-                # Display winning message
-                self.textColor = Board.COLOR_PLAYER_2
-                if resWin == 1:
-                    self.textColor = Board.COLOR_PLAYER_1
-                self.text = "Player " + str(self.state) + " won !"
-
-    def updatePossible(self, ix, iy):
-        print("UPDATE POSSIBLE", ix, iy)
-        if self.largeGrid[ix][iy] != 0: # If the cell is already won, play anywhere
-            print("Already WON")
-            self.possible = self.getAvailableLargeCells()
-        elif Board.gridIsFull(self.grid[ix][iy]): # If the cell is full play anywhere
-            print("Cell full")
-            self.possible = self.getAvailableLargeCells()
-        else: # The cell is not won and not full, play in this one
-            print("OK TP")
-            self.possible = [[False for _ in range(3)] for _ in range(3)]
-            self.possible[ix][iy] = True
-            
-    def getAvailableLargeCells(self):
-        available = [[True for _ in range(3)] for _ in range(3)]
-        for ix in range(3):
-            for iy in range(3):
-                if self.largeGrid[ix][iy] != 0: # If the cell is already won
-                    available[ix][iy] = False
-                elif Board.gridIsFull(self.grid[ix][iy]): # If the cell is full
-                    available[ix][iy] = False
-        return available
-
-    def getCellFromPx(px, py):
-        start = Board.getLargeTopLeftPx(0,0)
-        ixLarge = int((px - start[0]) // Board.getSizeLargeCell())
-        iyLarge = int((py - start[1]) // Board.getSizeLargeCell())
-        px2 = px - start[0] - ixLarge * Board.getSizeLargeCell()
-        py2 = py - start[1] - iyLarge * Board.getSizeLargeCell()
-        ixSmall = int(px2 // Board.getSizeSmallCell())
-        iySmall = int(py2 // Board.getSizeSmallCell())
-        return (ixLarge, iyLarge, ixSmall, iySmall)
-
-    def getSizeLargeCell():
-        return (Board.SIZE-Board.WIDTH_LARGE_GRID) / 3.
-
-    def getSizeSmallCell():
-        return (Board.SIZE-Board.WIDTH_LARGE_GRID) / 9.
-
-    def getLargeTopLeftPx(ix, iy):
-        px = 0.5*Board.WIDTH_LARGE_GRID + ix * Board.getSizeLargeCell() - 1
-        py = 0.5*Board.WIDTH_LARGE_GRID + iy * Board.getSizeLargeCell() - 1
-        return (px, py)
-
-    def getSmallTopLeftPx(ixLarge, iyLarge, ixSmall, iySmall):
-        (pxLarge, pyLarge) = Board.getLargeTopLeftPx(ixLarge, iyLarge)
-        px = pxLarge + ixSmall * Board.getSizeSmallCell()
-        py = pyLarge + iySmall * Board.getSizeSmallCell()
-        return (px, py)
-
-    def getSmallMidddlePx(ixLarge, iyLarge, ixSmall, iySmall):
-        (px, py) = Board.getSmallTopLeftPx(ixLarge, iyLarge, ixSmall, iySmall)
-        px += 0.5 * Board.getSizeSmallCell()
-        py += 0.5 * Board.getSizeSmallCell()
-        return (px, py)
-
-    def draw(self, blinkAvailableCells = True):
-        # Large grid background
-        s = Board.getSizeLargeCell()
-        for ix in range(3):
-            for iy in range(3):
-                gridValue = self.largeGrid[ix][iy]
-                pos = Board.getLargeTopLeftPx(ix, iy)
-                color = Board.COLOR_BACKGROUND
-                if gridValue == 1:
-                    color = Board.COLOR_BACKGROUND_PLAYER_1
-                elif gridValue == 2:
-                    color = Board.COLOR_BACKGROUND_PLAYER_2
-                elif blinkAvailableCells and (self.state == 0): # Blink possible moves
-                    if self.possible[ix][iy]:
-                        if current_milli_time() % Board.TIME_BLINK_AVAILABLE > 0.5 * Board.TIME_BLINK_AVAILABLE:
-                            color = Board.COLOR_BACKGROUND_AVAILABLE
-                pygame.draw.rect(screen, color, (pos[0], pos[1], s, s))
-
-        # Small grid lines
-        for i in range(10):
-            pos = 0.5 * Board.WIDTH_LARGE_GRID + (Board.SIZE - Board.WIDTH_LARGE_GRID) * i / 9. - 1
-            pygame.draw.line(screen, Board.COLOR_SMALL_GRID, (pos,0), (pos,Board.SIZE), width = Board.WIDTH_SMALL_GRID) # Vertical
-            pygame.draw.line(screen, Board.COLOR_SMALL_GRID, (0,pos), (Board.SIZE,pos), width = Board.WIDTH_SMALL_GRID) # Horizontal
-
-        # Large grid lines
-        for i in range(4):
-            pos = Board.getLargeTopLeftPx(i,0)[0]
-            pygame.draw.line(screen, Board.COLOR_LARGE_GRID, (pos,0), (pos,Board.SIZE), width = Board.WIDTH_LARGE_GRID) # Vertical
-            pygame.draw.line(screen, Board.COLOR_LARGE_GRID, (0,pos), (Board.SIZE,pos), width = Board.WIDTH_LARGE_GRID) # Horizontal
-
-        # Draw player moves
-        inc = 0.35 * Board.getSizeSmallCell()
-        for ixLarge in range(3):
-            for iyLarge in range(3):
-                for ixSmall in range(3):
-                    for iySmall in range(3):
-                        gridValue = self.grid[ixLarge][iyLarge][ixSmall][iySmall]
-                        pos = Board.getSmallMidddlePx(ixLarge, iyLarge, ixSmall, iySmall)
-                        if gridValue == 1: # Crosses
-                            pygame.draw.line(screen, Board.COLOR_PLAYER_1, (pos[0] - inc, pos[1] - inc), (pos[0] + inc, pos[1] + inc), width = Board.WIDTH_PLAYER_1)
-                            pygame.draw.line(screen, Board.COLOR_PLAYER_1, (pos[0] - inc, pos[1] + inc), (pos[0] + inc, pos[1] - inc), width = Board.WIDTH_PLAYER_1)
-                        elif gridValue == 2: # Circles
-                            pygame.draw.ellipse(screen, Board.COLOR_PLAYER_2, (pos[0] - inc, pos[1] - inc, 2*inc, 2*inc), width = Board.WIDTH_PLAYER_2)
-
-        # Text
-        pygame.draw.rect(screen, Board.COLOR_BACKGROUND, (0, Board.SIZE, Board.SIZE, Board.BOTTOM_SIZE))
-        text = Board.FONT.render(self.text, True, self.textColor)
-        text_rect = text.get_rect(center=(Board.SIZE / 2., Board.SIZE + Board.BOTTOM_SIZE / 2.))
-        screen.blit(text, text_rect)
-
-
-
 screen = pygame.display.set_mode((Board.SIZE, Board.SIZE + Board.BOTTOM_SIZE))
 board = Board()
+
 
 if __name__ == '__main__':
 
     game = True
-    while game:
+    while game: # While the game is running
         pygame.time.delay(1)
 
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type == pygame.QUIT: # Quit cross
                 game = False
-            if event.type == pygame.MOUSEBUTTONUP:
+            if event.type == pygame.MOUSEBUTTONUP: # Mouse click released
                 x, y = pygame.mouse.get_pos()
                 board.click(x, y)
 
-        # Draws things on screen
-
-        board.draw()
-
-        # Updates movements and events
-
         # Update display
-
+        board.draw(screen)
         pygame.display.flip()
 
-    pygame.QUIT()
+    pygame.quit()
