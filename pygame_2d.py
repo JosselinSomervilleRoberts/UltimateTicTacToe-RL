06fd6a5de425896f0187bc00148f74b0b5166454
JosselinSomervilleRoberts <josselin.somerville@gmail.com>
2022-02-25 12:41:48 +0100

    Add play methods and win checks

--- pygame_2d.py
+++ pygame_2d.py
@@ -23,12 +23,60 @@ class Board:
     COLOR_BACKGROUND_PLAYER_1 = (255, 150, 150)
     COLOR_BACKGROUND_PLAYER_2 = (150, 150, 255)
 
+
+    def checkWinBoard(grid):
+        for i in range(3):
+            if grid[i][0] == grid[i][1] == grid[i][2] != 0:
+                return grid[i][0]
+            if grid[0][i] == grid[1][i] == grid[2][i] != 0:
+                return grid[0][i]
+
+        # Diagonals
+        if grid[0][0] == grid[1][1] == grid[2][2] != 0:
+            return grid[0][0]
+        if grid[0][2] == grid[1][1] == grid[2][0] != 0:
+            return grid[0][2]
+
+        return 0
+
     def __init__(self):
+        self.reset()
+
+    def reset(self):
         self.resetGrid()
+        self.currentPlayer = 1
+        self.state = 0
+        self.possible = [[True for _ in range(3)] for _ in range(3)]
 
     def resetGrid(self):
         self.grid = np.zeros((3,3,3,3), dtype=int).tolist()
         self.largeGrid = np.zeros((3,3), dtype=int).tolist()
+
+    def play(self, ixLarge, iyLarge, ixSmall, iySmall):
+        if self.state != 0:
+            return 1 # Move not aload since the game is already finished
+        if not(self.possible[ixLarge][iyLarge]):
+            return 2 # Move not aload since not in the correct large cell
+        elif self.grid[ixLarge][iyLarge][ixSmall][iySmall] != 0:
+            return 3 # Move not aload since the small cell is already occupied
+
+        self.grid[ixLarge][iyLarge][ixSmall][iySmall] = self.currentPlayer # Play
+        self.currentPlayer = 3 - self.currentPlayer # Swap player
+        self.checkWinLargeCell(ixLarge, iyLarge)
+        self.updatePossible(ixSmall, iySmall)
+
+    def checkWinLargeCell(self, ix, iy):
+        g = self.grid[ix][iy]
+        res = Board.checkWinBoard(g)
+        if res != 0:
+            self.largeGrid[ix][iy] = res
+            resWin = Board.checkWinBoard(self.largeGrid)
+            if resWin != 0: # A player won
+                self.state = resWin
+                # TODO: Display a winning message
+
+    def updatePossible(self, ix, iy):
+        pass
 
     def getSizeLargeCell(self):
         return (boardSize-self.WIDTH_LARGE_GRID) / 3.
@@ -66,8 +114,6 @@ class Board:
                 elif gridValue == 2:
                     color = self.COLOR_BACKGROUND_PLAYER_2
                 pygame.draw.rect(screen, color, (pos[0], pos[1], pos[0] + s, pos[1] + s))
-                
-
 
         # Small grid lines
         for i in range(10):
