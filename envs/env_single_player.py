8b53033934dac4ed75fe04cfec2f9d26407409f3
JosselinSomervilleRoberts <josselin.somerville@gmail.com>
2022-02-27 16:14:21 +0100

    Add single player env

---
+++ envs/env_single_player.py
@@ -0,0 +1,35 @@
+from envs.env_two_player import TwoPlayerEnv
+
+class SinglePlayerEnv(TwoPlayerEnv):
+
+    def __init__(self, agent2, randomOrder=False):
+        super().__init__()
+        self.agent2 = agent2
+        self.randomOrder = randomOrder
+
+    def reset(self):
+        self.last_obs = super().reset()
+        self.last_done = False
+        return self.last_obs
+
+    def step(self, action):
+        self.pygame.do_action(action)
+        reward = self.pygame.evaluate()
+
+        if self.pygame.error > 0: # There is an error, the move could not be played
+            return self.last_obs, reward, self.last_done, {}
+        else: # The move was played so we make the opponent play
+            player_opponent = self.pygame.board.currentPlayer
+            obs = self.pygame.observe()
+            while self.pygame.board.currentPlayer == player_opponent:
+                action = self.agent2.getAction(self, obs)
+
+                # if the action is valid we play it in the env
+                if action < 81: self.pygame.do_action(action)
+
+            self.last_obs = self.pygame.observe()
+            reward2 = self.pygame.evaluate()
+            self.last_done = self.pygame.is_done()
+
+            # Here we return as the reward the reward of the agent move - the reward of his opponent for his move
+            return self.last_obs, reward + reward2, self.last_done, {}
---
+++ play_modes/agent_in_single_player_env.py
@@ -0,0 +1,53 @@
+
+'''
+# necessary for Marie
+import sys
+sys.path.append("C:\\Users\\Marie\\Organisation_Marie\\X\\3A\\INF 581 - Advanced machine learning\\Project\\UltimateTicTacToe-RL")
+'''
+
+from envs.env_single_player import SinglePlayerEnv
+import pygame
+
+from agents.agent_player import PlayerAgent
+from agents.agent_random import RandomAgent
+from agents.agent_minimax import MinimaxAgent
+from agents.agent_minimax_pruning import MinimaxPruningAgent
+
+agent = PlayerAgent(1)
+display = True
+
+if __name__ == '__main__':
+    agent2 = MinimaxPruningAgent(2, 6, True)
+    env = SinglePlayerEnv(agent2)
+    obs = env.reset()
+
+    done = False
+    while not(done):
+
+        # Ask the agent to choose an action
+        action = agent.getAction(env, obs)
+
+        # If the aciton is negative this means that the agent asks to close the game
+        if action < 0:
+            done = True
+
+        # Otherwise, if the action is valid we play it in the env
+        elif action < 81:
+            obs, reward, done, info = env.step(action)
+            print("REWARD:", reward)
+
+        if display:
+            # Render the environment
+            env.render()
+
+            # Check for pygame event (to close the window)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT: # Quit cross
+                    done = True
+
+            # Delay to not spam
+            pygame.time.delay(1)
+
+    pygame.quit()
+    print("ENV state:", env.pygame.board.state)
+    env.close()
